95165d3466cc4f8d0695674dffa004c83b90d020
yoichi kobayashi <info@tplh.net>
2016-11-18 03:03:11 +0900

    [add] create functions moving page with ajax and setting pushstate.

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,47 @@
 import initCommon from './init/common.js'
 import initIndex from './init/index.js'
+import initPage1 from './init/page1.js'
+import initPage2 from './init/page2.js'
+import initPage3 from './init/page3.js'
 
 const { pathname } = window.location;
 
 const init = () => {
-  initCommon();
-  switch (pathname.replace('index.html', '')) {
-    case '/':
-      initIndex();
-      break;
-    default:
+  history.replaceState('movePage', null, location.pathname);
+  const classNameContents = '.l-contents';
+  const $contents = $(classNameContents);
+  const loadContents = (href) => {
+    $.ajax({
+      url: href,
+      type: 'GET',
+      dataType: 'html',
+      cache: true
+    })
+    .done(function(data) {
+      console.log("success");
+      const html = $.parseHTML(data);
+      //console.log(html);
+      const $loadContents = $('<div/>').append(html).find(classNameContents);
+      $contents.html($loadContents);
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      console.log("complete");
+    });
   }
+  $('a.js-pjax').on('click', function(event) {
+    event.preventDefault();
+    const href = $(this).attr('href');
+    if (href == location.pathname) return;
+    loadContents(href);
+    history.pushState('movePage', null, href);
+  });
+  $(window).on('popstate', function(event){
+    if (event.state == 'movePage') {
+      loadContents(location.pathname);
+    }
+  });
 }
 init();
