a9f6c030186dd1c7a30d8ea9d41786cde227c827
yoichi kobayashi <info@tplh.net>
2016-11-18 14:24:20 +0900

    [add] created Pjax Class.

--- src/js/main.js
+++ src/js/main.js
@@ -6,55 +6,61 @@ import initPage3 from './init/page3.js'
 
 const { pathname } = window.location;
 
-const classNameLink = 'a.js-pjax';
-const classNameContents = '.l-contents';
-const $contents = $(classNameContents);
-let htmlLoaded = null;
-let $contentsLoaded = null;
+const Pjax = class Pjax {
+  constructor(classNameLink, classNameContents) {
+    this.classNameLink = classNameLink;
+    this.classNameContents = classNameContents;
+    this.$contents = $(classNameContents);
+    this.htmlLoaded = null;
+    this.$contentsLoaded = null;
 
-const loadContents = (href) => {
-  $.ajax({
-    url: href,
-    type: 'GET',
-    dataType: 'html',
-    cache: true
-  })
-  .done(function(data) {
-    console.log("success");
-    htmlLoaded = null;
-    $contentsLoaded = null;
-    htmlLoaded = $.parseHTML(data);
-    $contentsLoaded = $('<div/>').append(htmlLoaded).find(classNameContents);
-    $contents.empty();
-    $contentsLoaded.find(classNameLink).on('click.pjax', function(event) {
-      onClickPjax(event, $(this))
+    this.init();
+  }
+  init() {
+    const _this = this;
+    history.replaceState('movePage', null, location.pathname);
+    $(this.classNameLink).on('click.pjax', function(event) {
+      _this.onClickPjax(event, $(this))
+    })
+    $(window).on('popstate.pjax', function(event) {
+      if (event.state == 'movePage') {
+        _this.loadContents(location.pathname);
+      }
     });
-    $contents.html($contentsLoaded);
-  })
-  .fail(function() {
-    console.log("error");
-  })
-  .always(function() {
-    console.log("complete");
-  });
+  }
+  loadContents(href) {
+    const _this = this;
+    $.ajax({
+      url: href,
+      type: 'GET',
+      dataType: 'html',
+      cache: true
+    })
+    .done((data) => {
+      console.log("success");
+      this.htmlLoaded = null;
+      this.$contentsLoaded = null;
+      this.htmlLoaded = $.parseHTML(data);
+      this.$contentsLoaded = $('<div/>').append(this.htmlLoaded).find(this.classNameContents);
+      this.$contents.empty();
+      this.$contentsLoaded.find(this.classNameLink).on('click.pjax', function(event) {
+        _this.onClickPjax(event, $(this))
+      });
+      this.$contents.html(this.$contentsLoaded);
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      console.log("complete");
+    });
+  }
+  onClickPjax(event, $this) {
+    event.preventDefault();
+    const href = $this.attr('href');
+    if (href == location.pathname) return;
+    this.loadContents(href);
+    history.pushState('movePage', null, href);
+  }
 }
-const onClickPjax = (event, $this) => {
-  event.preventDefault();
-  const href = $this.attr('href');
-  if (href == location.pathname) return;
-  loadContents(href);
-  history.pushState('movePage', null, href);
-}
-
-const init = () => {
-  history.replaceState('movePage', null, location.pathname);
-  $(classNameLink).on('click.pjax', function(event) {
-    onClickPjax(event, $(this))
-  })
-  $(window).on('popstate.pjax', function(event){
-    if (event.state == 'movePage') {
-      loadContents(location.pathname);
-    }
-  });
-}
-init();
+const pjax = new Pjax('a.js-pjax', '.l-contents');
