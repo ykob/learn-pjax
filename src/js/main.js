96d07db28885e7c4de4a61914dc8bba2b8761741
yoichi kobayashi <info@tplh.net>
2016-11-18 14:31:33 +0900

    [add] modularized Pjax Class.

--- src/js/main.js
+++ src/js/main.js
@@ -4,63 +4,8 @@ import initPage1 from './init/page1.js'
 import initPage2 from './init/page2.js'
 import initPage3 from './init/page3.js'
 
-const { pathname } = window.location;
+import Pjax from './modules/Pjax.js'
 
-const Pjax = class Pjax {
-  constructor(classNameLink, classNameContents) {
-    this.classNameLink = classNameLink;
-    this.classNameContents = classNameContents;
-    this.$contents = $(classNameContents);
-    this.htmlLoaded = null;
-    this.$contentsLoaded = null;
+// const { pathname } = window.location;
 
-    this.init();
-  }
-  init() {
-    const _this = this;
-    history.replaceState('movePage', null, location.pathname);
-    $(this.classNameLink).on('click.pjax', function(event) {
-      _this.onClickPjax(event, $(this))
-    })
-    $(window).on('popstate.pjax', function(event) {
-      if (event.state == 'movePage') {
-        _this.loadContents(location.pathname);
-      }
-    });
-  }
-  loadContents(href) {
-    const _this = this;
-    $.ajax({
-      url: href,
-      type: 'GET',
-      dataType: 'html',
-      cache: true
-    })
-    .done((data) => {
-      console.log("success");
-      this.htmlLoaded = null;
-      this.$contentsLoaded = null;
-      this.htmlLoaded = $.parseHTML(data);
-      this.$contentsLoaded = $('<div/>').append(this.htmlLoaded).find(this.classNameContents);
-      this.$contents.empty();
-      this.$contentsLoaded.find(this.classNameLink).on('click.pjax', function(event) {
-        _this.onClickPjax(event, $(this))
-      });
-      this.$contents.html(this.$contentsLoaded);
-    })
-    .fail(function() {
-      console.log("error");
-    })
-    .always(function() {
-      console.log("complete");
-    });
-  }
-  onClickPjax(event, $this) {
-    event.preventDefault();
-    const href = $this.attr('href');
-    if (href == location.pathname) return;
-    this.loadContents(href);
-    history.pushState('movePage', null, href);
-  }
-}
 const pjax = new Pjax('a.js-pjax', '.l-contents');
